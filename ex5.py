8df790388e8bde55c26ffd255e8f9de699b7577a
dhanansezhiyan4-commits <dhanansezhiyan4@gmail.com>
2025-07-29 10:13:15 +0530

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,17 @@
+print("dhanansezhiyan interationl [p] lte")
+print("no 15 nagapara dist,bangalore")
+print("----------------------------------")
+print("empleye payroll system")
+print("----------------------------------")
+id=int(input("enter the empoyee id:"))
+name=input("enter the empoyee name:")
+salary=int(input("enter the salary:"))
+print("income")
+bonas=salary*20/100
+print("bonas(20 paracentage):",bonas)
+overtime=salary*10/100
+print("overtime(10 paracentage):",overtime)
+ta=salary*15/100
+print("trauel allowance(15 paracentage):",ta)
+grass=salary+bonas+overtime+ta
+print("grass paid in pupees:",grass)
