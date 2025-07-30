4babcbe55a9b7aaa300e9b513cee8d6349634cff
dhanansezhiyan4-commits <dhanansezhiyan4@gmail.com>
2025-07-30 16:04:41 +0530

    Add files via upload

---
+++ ex6.py
@@ -0,0 +1,14 @@
+print("Squence and Rectange")
+print("--------------------")
+a=int(input("Enter the a Value:"))
+b=int(input("Enter the b Value:"))
+l=int(input("Enter the l Value:"))
+print("Result")
+ps=4*a
+print("Perimeter of Suence:",ps)
+ae=a*a
+print("Aera of Squence:",ae)
+pr=2*(l+b)
+print("Perimeter of Rectangle:",pr)
+ar=l*b
+print("Area of Rectangle:",ar)
---
+++ ex7.py
@@ -0,0 +1,8 @@
+print("Circle Program")
+print("--------------")
+radius=int(input("Enter the Radius Value:"))
+print("Result")
+pr=2*22/7*radius
+print("Perimeter of Circle:",pr)
+ar=22/7*radius*radius
+print("Area of Circle:",ar)
