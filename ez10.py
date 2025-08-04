9cdd6c7f13e7c3b78c643fdd3836a6be449bd6ab
dhana <dhanansezhiyan4@gmail.com>
2025-08-04 10:47:33 +0530

    Add files via upload

---
+++ ez10.py
@@ -0,0 +1,14 @@
+print("Exponential Program")
+print("-------------------")
+x=int(input("Enter the x Value:"))
+y=int(input("Enter the y Value:"))
+z=int(input("Enter the z Value:"))
+print("Result")
+p=x*x+y*y+z*z
+print("p:",p)
+q=x*x*x+y*y+z+20
+print("q:",q)
+r=x*x*y+y*y+z*z*x+10
+print("r",r)
+s=x*x*x+y*y*y+z*z*+y*y*+x*y*z
+print("s:",s)
